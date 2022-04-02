bbad2ab4761d9312e713093714b59bac78be7a2c
FangtaoJI <fangtao.ji@my.jcu.edu.au>
2022-04-02 15:33:24 +0800

    add a password checking function

--- password_check.py
+++ password_check.py
@@ -1,10 +1,22 @@
-def is_password(text):
-    return 8 <= len(text) <= 20
+"""
+Name:Fantao ji
+"""
+MIN_LENGTHEN = 8
+MAX_LENGTHEN = 20
+
+
+def check_length():
+    get_password = input("Enter your password")
+    while len(get_password) < MIN_LENGTHEN or len(get_password) > MAX_LENGTHEN:
+        print("Invalid input")
+        get_password = input("Enter your password")
+    else:
+        return get_password
 
 
 def main():
-    new_password = "hollowed"
-    print("{} is valid password".format(new_password))
+    password=check_length()
+    print("*" * len(password))
 
 
 if __name__ == '__main__':
