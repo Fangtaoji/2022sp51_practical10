39acc1d807137b6af00694b67969e790278690f5
Fangtaoji <fangtao.ji@my.jcu.edu.au>
2022-05-06 14:22:59 +0800

    lets the user enter and clear their name, and greets them

---
+++ practical07/box_layout_demo.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 Practical
+Kivy GUI program to square a number
+Lindsay Ward, IT@JCU
+Started 13/10/2015
+"""
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        print('greet')
+        print("test")
+        self.root.ids.output_label.text = "Hello "
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+    def clear(self):
+        self.root.ids.input_name.text = " "
+        self.root.ids.output_label.text = "Hello"
+
+
+BoxLayoutDemo().run()
